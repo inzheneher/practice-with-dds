968f8b39670d5c56f9adbaeaea0a2da7d308596b
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2025-05-06 20:02:38 -0400

    Add BinaryTreeRightSideView and test for it

--- src/main/java/199_BinaryTreeRightSideView.java
+++ src/main/java/199_BinaryTreeRightSideView.java
@@ -1,13 +1,63 @@
 /*
-* https://leetcode.com/problems/binary-tree-right-side-view/description/
-* */
+ * https://leetcode.com/problems/binary-tree-right-side-view/description/
+ * */
 
 import common.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 class BinaryTreeRightSideView {
+
     public List<Integer> rightSideView(TreeNode root) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        TreeNode node;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                node = queue.poll();
+
+                if (node != null) {
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+
+                    if (i == size - 1) {
+                        result.add(node.val);
+                    }
+                }
+            }
+        }
+
+        return result;
     }
+
+    public List<Integer> rightSideViewRec(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, result, 0);
+        return result;
+    }
+
+    private void dfs(TreeNode root, List<Integer> result, int level) {
+        if (root == null) {
+            return;
+        }
+        if (level == result.size()) {
+            result.add(root.val);
+        }
+
+        dfs(root.right, result, level + 1);
+        dfs(root.left, result, level + 1);
+    }
+
 }
--- src/test/java/BinaryTreeRightSideViewTest.java
+++ src/test/java/BinaryTreeRightSideViewTest.java
@@ -19,5 +19,7 @@ class BinaryTreeRightSideViewTest {
     void rightSideView() {
         assertEquals(Arrays.asList(1, 3, 4), binaryTree.rightSideView(buildTree(new Integer[]{1, 2, 3, null, 5, null, 4})));
         assertEquals(Arrays.asList(1, 3, 4, 5), binaryTree.rightSideView(buildTree(new Integer[]{1, 2, 3, 4, null, null, null, 5})));
+        assertEquals(Arrays.asList(1, 3, 4), binaryTree.rightSideViewRec(buildTree(new Integer[]{1, 2, 3, null, 5, null, 4})));
+        assertEquals(Arrays.asList(1, 3, 4, 5), binaryTree.rightSideViewRec(buildTree(new Integer[]{1, 2, 3, 4, null, null, null, 5})));
     }
 }
