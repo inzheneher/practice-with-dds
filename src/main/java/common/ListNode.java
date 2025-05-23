2ac83fe14341728f2f73ef1f8a2dbb1e41af67f3
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2025-05-22 22:11:47 -0400

    Update ListNode

--- src/main/java/common/ListNode.java
+++ src/main/java/common/ListNode.java
@@ -15,4 +15,44 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        ListNode a = this, b = (ListNode) obj;
+
+        while (a != null && b != null) {
+            if (a.val != b.val) return false;
+            a = a.next;
+            b = b.next;
+        }
+        return a == null && b == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        ListNode current = this;
+        while (current != null) {
+            result = 31 * result + current.val;
+            current = current.next;
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        ListNode current = this;
+        sb.append("[");
+        while (current != null) {
+            sb.append(current.val);
+            if (current.next != null) sb.append(" -> ");
+            current = current.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
